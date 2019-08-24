1c385ab46724f4fcf11a623d88e1d64d477789e9
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-08-24 20:38:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function search() {
     const city = document.querySelector('input').value;
 
-    axios.get(`https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=99bf340ab3242744dbfe45645c790301`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=99bf340ab3242744dbfe45645c790301`)
         .then(function(response) {
             // handle success
             console.log(response);
