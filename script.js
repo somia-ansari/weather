f2972bfbf042a0149c4588b7ae316a43c6655770
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-08-24 18:33:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function search() {
+    const city = document.querySelector('input').value;
+
+    axios.get(`https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=99bf340ab3242744dbfe45645c790301`)
+        .then(function(response) {
+            // handle success
+            console.log(response);
+            document.querySelector('h1').innerHTML = `City Name: ${response.data.name}`;
+            document.querySelector('p').innerHTML = `Weather: ${response.data.weather[0].main} <br/>
+        Description: ${response.data.weather[0].description}`;
+
+        })
+        .catch(function(error) {
+            // handle error
+            document.querySelector('h1').innerHTML = `Error!! Wrong input`;
+
+        })
+        .finally(function() {
+            // always executed
+        });
+}
