b8e248d680c294ca2b368b3accaaa288f84babd6
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-08-27 00:07:15 +0500

    wip-ui

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ function search() {
         .then(function(response) {
             // handle success
             console.log(response);
-            document.querySelector('h1').innerHTML = `City Name: ${response.data.name}`;
-            document.querySelector('p').innerHTML = `Weather: ${response.data.weather[0].main} <br/>
-        Description: ${response.data.weather[0].description}`;
+
+            document.querySelector('#weather h1').innerHTML = `City Name: ${response.data.name}`;
+            document.querySelector('#weather-status').innerHTML = `Weather: ${response.data.weather[0].main}`;
+            document.querySelector('#weather-desc').innerHTML = `Description: ${response.data.weather[0].description}`;
+            document.querySelector('#temperature').innerHTML = `Temperature in Celsius: ${Math.floor(response.data.main.temp-273.15)}deg C`;
+            document.querySelector('#wind-speed').innerHTML = `Wind Speed: ${response.data.wind.speed} min/sec`;
+
+            document.querySelector('#time').innerHTML = `Time: ${new Date(response.data.dt * 1000).getHours()}:${new Date(response.data.dt * 1000).getMinutes()}:${new Date(response.data.dt * 1000).getSeconds()}`;
+            document.querySelector('#date').innerHTML = `Date: ${new Date(response.data.dt * 1000).getDate()} of ${new Date(response.data.dt * 1000).getMonth()}th ${new Date(response.data.dt * 1000).getFullYear()}`;
+
+
+
 
         })
         .catch(function(error) {
             // handle error
-            document.querySelector('h1').innerHTML = `Error!! Wrong input`;
+            document.querySelector('#weather h1').innerHTML = `Error!! Wrong input`;
+            document.querySelector('#weather-status').innerHTML = "";
+
 
         })
         .finally(function() {
